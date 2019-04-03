095c14f0004192355325766c0b92832098e650a0
shizhonghao <shi1996426frank@126.com>
2019-04-03 14:50:56 -0700

    add data base forms for the judge

--- src/apis/judge/router.py
+++ src/apis/judge/router.py
@@ -1,7 +1,30 @@
 from flask import Blueprint
+from apis.models import User, OnlineJudge, Problem, Submission, Contest
 
 judge = Blueprint('judge', __name__)
 
-@judge.route('/contest_list')
-def contest_list():
+@judge.route('/contest')
+def contest():
 	return "list here"
+
+@judge.route('/test')
+def test():
+	test_user = User(name='test',password='password', email='test@test.com')
+	test_user.save()
+	test_problem = Problem()
+	oj = OnlineJudge(name='TestJudge')
+	oj.save()
+	return "problem here"
+
+@judge.route('/submission')
+def submission():
+	return "show submission"
+
+@judge.route('/submit')
+def submit():
+	return "submit"
+
+@judge.route('/new_contest')
+def new_contest():
+	return "new contest"
+
--- src/apis/models.py
+++ src/apis/models.py
@@ -1,10 +1,49 @@
 from flask_mongoengine import MongoEngine
 from flask_login import UserMixin
 from . import db
+from mongoengine import *
+
 
 # user model
 class User(UserMixin, db.Document):
-    username = db.StringField(max_length=32, required=True, unique=True)
+    name = db.StringField(max_length=32, required=True, unique=True)
     password = db.StringField(max_length=255, required=True)
     email = db.StringField(max_length=255)
     is_admin = db.BooleanField(default=False)
+    submission_list = db.ListField(db.ReferenceField('Submission'))
+
+
+class OnlineJudge(db.Document):
+    name = db.StringField(required=True, unique=True)
+    oj_id = db.IntField(required=True, unique=True)
+
+
+class Problem(db.Document):
+    online_judge = db.ReferenceField('OnlineJudge')
+    problem_id = db.StringField(required=True, unique=True)
+    title = db.StringField(required=True)
+    description = db.StringField()
+    input_format = db.StringField()
+    output_format = db.StringField()
+    sample_input = db.StringField()
+    sample_output = db.StringField()
+
+
+class Submission(db.Document):
+    user = db.ReferenceField('User')
+    problem = db.ReferenceField('Problem')
+    submission_time = db.DateTimeField()
+    submission_status = db.StringField()
+    language = db.StringField()
+    solution = db.StringField()
+
+
+class Contest(db.Document):
+    contest_name = db.StringField()
+    organizer = db.ReferenceField('User')
+    problem_list = db.ListField(db.ReferenceField('Problem'))
+    participant_list = db.ListField(db.ReferenceField('User'))
+    submission_list = db.ListField(db.ReferenceField('Submission'))
+    start_time = db.DateTimeField()
+    end_time = db.DateTimeField()
+
